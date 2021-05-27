c0828232e9b56b4d2ef3d2121f2aedfbe152eb9d
cmonsalt <cristiantm12@gmail.com>
2021-05-26 19:42:40 -0500

    Fin menu

---
+++ menu.js
@@ -0,0 +1,9 @@
+const btnMenu = document.querySelector("#btnMenu");
+const menu = document.querySelector("#menu");
+btnMenu.addEventListener("click", function(){
+ menu.classList.toggle("mostrar");
+});
+
+
+
+
